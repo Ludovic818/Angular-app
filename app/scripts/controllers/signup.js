cdcbb2a6d102a54dd4e3c3a11134b9c000a21851
ludovic818 <l.marchand818@laposte.net>
2016-03-02 11:30:45 +0100

    created signup form

---
+++ app/scripts/controllers/signup.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularAppApp.controller:SignupCtrl
+ * @description
+ * # SignupCtrl
+ * Controller of the angularAppApp
+ */
+angular.module('angularAppApp')
+  .controller('SignupCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
